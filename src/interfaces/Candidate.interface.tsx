20343fa22afcbaec1db072176d8d113bd4fa04d5
victoriamata <victoriamataxo@gmail.com>
2024-11-04 17:54:36 -0500

    Added CandidateSearch function to search username, and got code to run on local host

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,10 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+interface Candidate {
+  id: number;                  // Unique identifier for the candidate
+  name: string;               // Full name of the candidate
+  location: string;            // Candidate location
+  email: string;               // Contact email
+  company: string;             // Company name
+  bio: string;                 // Candidate personal bio
+  status: boolean;             // Rejected or approved
+}
--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,8 +1,46 @@
 import { useState, useEffect } from 'react';
-import { searchGithub, searchGithubUser } from '../api/API';
+import { searchGithubUser } from '../api/API';
 
 const CandidateSearch = () => {
-  return <h1>CandidateSearch</h1>;
+  const [candidates, setCandidates] = useState<Candidate[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [username, setUsername] = useState("someUsername");
+
+  useEffect(() => {
+    const fetchCandidates = async () => {
+      try {
+        const response = await searchGithubUser(username);
+        setCandidates(response);
+      } catch (error) {
+        console.error("Error fetching candidates:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCandidates();
+  }, [username]);
+
+  return (
+    <div>
+      <h1>Candidate Search</h1>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)} // Use setUsername to update the username
+        placeholder="Enter username"
+      />
+      {loading ? (
+        <p>Loading...</p> ):(
+      <ul>
+        {candidates.map((candidate) => (
+          <li key={candidate.id}>{candidate.name}</li>
+        ))}
+      </ul>
+        )}
+    </div>
+  );
 };
 
 export default CandidateSearch;
+
